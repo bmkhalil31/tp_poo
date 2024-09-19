e185ba1580cbf9ea717937b6462f67c19beb4536
mkhalil26 <130088294+mkhalil26@users.noreply.github.com>
2024-09-19 15:14:33 +0200

    test classes

---
+++ src/personnages/Druide.java
@@ -0,0 +1,29 @@
+package personnages;
+
+public class Druide {
+
+	private String nom;
+	private int effetPotionMin;
+	private int effetPotionMax;
+
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<<" + texte + ">>");
+
+	}
+
+	private String prendreParole() {
+		// TODO Auto-generated method stub
+		return "Le druide " + nom + ":";
+	}
+
+}
